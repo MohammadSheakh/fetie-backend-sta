67a08adabf6d9d2cbc58bbc6808e1991cfe04f73
Mohammad Sheakh <mohammad.sheakh@gmail.com>
2025-05-09 08:51:29 +0600

    unnessary comment removed

--- src/modules/helpMessage/helpMessage.controller.ts
+++ src/modules/helpMessage/helpMessage.controller.ts
@@ -81,48 +81,5 @@ export class HelpMessageController extends GenericController<
   });
 
   // add more methods here if needed or override the existing ones
-  /*
-  convnext achived the highest scores across all metrics, highlighting
-  its superior ability to accurately classify coffee leaf diseases. 
-
-  Vision transformer and swin transformer, though effective, were 
-  less practical for real time scenarios due to their high computational
-  demands.  
-
-  VGG-19, despite its established history, struggled with overfitting
-  and exhibited lower accuracy, further showcasing the advantages of 
-  convNext.
-
-
-
-  convNext performance was compared to existing methods .  
-  A Study by hasan et al, in 2022 he achived 90% accuracy using color processing technique.
-  but struggled with precision and scalability. Novtahaning et al utilized ensemble learning
-  and achived 97.31%  but the approach required significant computational resources. 
   
-  yamashita and leite in 2023 proposed edge- based models that achived 98% accuracy
-  but these were limited in their adaptability to diverse datasets. 
-
-  in 2022 paulos achived 99% accuracy with res net 50; however overfitting 
-  issues were observed with smaller datasets.
-
-  in contrast, convNext delivered consistently superior accuracy and adaptability, 
-  addressing challenges faced by these methods .
-
-
-  conclusion : 
-
-  Superior performance
-  Scalability and Adaptability
-  Practicality for Real world use
-  impact on agriculture 
-
-  Future Work:
-  Expand to other crops
-  improve interpretability
-  optimize for edge devices
-  sustainability focus
-  global scalability 
-
-  */
 }
