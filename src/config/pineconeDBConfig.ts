1669a35a342292a561e21417b88f5d643d65fc58
Mohammad Sheakh <mohammad.sheakh@gmail.com>
2025-06-03 11:26:14 +0600

    remove pinecone db config :fire:

--- src/config/pineconeDBConfig.ts
+++
@@ -1,9 +0,0 @@
-import { Pinecone } from "@pinecone-database/pinecone";
-import { v4 as uuidv4 } from "uuid";
-const pinecone = new Pinecone({
-  apiKey:
-    process.env.PINECONE_API ??
-    (() => {
-      throw new Error("PINECONE_API environment variable is not set");
-    })(),
-});
